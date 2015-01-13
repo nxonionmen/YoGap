fb57dce39c6a6530e661e36acb29ea932d83da1d
Kyungjae Park <kyungjae.park@gmail.com>
2015-01-13 13:28:15 +0900

    flip 샘플 추가.

---
+++ sample/flip/common.js
@@ -0,0 +1,9 @@
+// http://stackoverflow.com/questions/1744310/how-to-fix-array-indexof-in-javascript-for-internet-explorer-browsers
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function(obj, start) {
+         for (var i = (start || 0), j = this.length; i < j; i++) {
+             if (this[i] === obj) { return i; }
+         }
+         return -1;
+    }
+}
