898fe7bc634ec508fe1bba83ca2cf396794f2e33
Kyungjae Park <kyungjae.park@gmail.com>
2015-01-15 23:00:39 +0900

    parse init 코드 추가

--- members/kyungjae/week2/hi.js
+++ members/kyungjae/week2/hi.js
@@ -4,6 +4,14 @@ function main()
 {
 	createBasicButtons();
 	flip("page_welcome", true);
+
+	Parse.initialize("2MyosBYZSSz1F4kzJwCtBGyupBdlPOVM60K1fBiM", "ahe7CcO6A39j9nFHd5hZsuYuLM6WivCaNLPviHS0");
+	var TestObject = Parse.Object.extend("TestObject");
+	var testObject = new TestObject();
+		testObject.save({foo: "bar"}).then(function(object) {
+		alert("yay! it worked");
+	});
+
 }
 
 function createBasicButtons()
